2988238680c701b7a0510aa622e26ee62b3d71b0
Tejas Bhovad <tejasbhovad@gmail.com>
2023-01-25 13:19:52 +0530

    init commit

---
+++ docs/intro.md
@@ -0,0 +1,26 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Introduction
+
+Find all the **Programs** you are looking for here
+
+The documentation will contain all the programs organized category-wise
+
+## Compilation
+
+### Linux
+
+1. Open Terminal
+2. `cd Desktop/` - change the current working directory to Desktop
+3. `touch filename.c` - create a file named `filename.c` in current working directory
+4. `gcc filename.c -o out` - compiling the `filename.c` file in binary file `out`
+5. `./out` - to run the compiled program
+
+:::note
+
+If your program uses Math functions use `gcc filename.c -o out -lm` instead
+
+:::
---
+++ docusaurus.config.js
@@ -0,0 +1,123 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Docs",
+  tagline: "Documentation for Engineering Programs",
+  url: "https://tejasbhovad.github.io",
+  baseUrl: "/docs",
+  trailingSlash: false,
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "TejasBhovad", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  plugins: ["docusaurus-plugin-sass"],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "<docs>",
+        // logo: {
+        //   alt: "My Site Logo",
+        //   src: "img/logo.svg",
+        // },
+        items: [
+          // {
+          //   type: "doc",
+          //   docId: "intro",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
+          // { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/TejasBhovad/docs",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Home",
+                to: "/",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Github",
+                href: "https://github.com/TejasBhovad",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/tejas_bhovad",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Website",
+                href: "https://tejasbhovad.vercel.app",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Tejas Bhovad`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
+// (module.exports = {
+//     config
+//     plugins: ["docusaurus-plugin-sass"],
+//   });
