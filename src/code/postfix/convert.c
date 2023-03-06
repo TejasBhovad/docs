d58f23146afdc9ccbd7616aa658f1f78b615e83c
Tejas Bhovad <tejasbhovad@gmail.com>
2023-03-06 23:58:51 +0530

    updated journal docs

--- src/code/postfix/convert.c
+++ src/code/postfix/convert.c
@@ -4,101 +4,94 @@
 char stack[100];
 int top = -1;
 
-void
-push (char x)
+void push(char x)
 {
   top++;
   stack[top] = x;
 }
 
-void
-display (char *ptr)
+void display(char *ptr)
 {
-  printf ("\nElements in Stack:\n");
+  printf("\nElements in Stack:\n");
   for (int i = 0; i <= top; i++)
-    {
-      printf (" %d", *(ptr + i));
-    }
+  {
+    printf(" %d", *(ptr + i));
+  }
 }
 
-char
-pop ()
+char pop()
 {
   if (top == -1)
-    {
-      printf ("\nUnderflow Error");
-      return -1;
-    }
+  {
+    printf("\nUnderflow Error");
+    return -1;
+  }
 
   else
-    {
+  {
 
-      return stack[top--];
-    }
+    return stack[top--];
+  }
 }
 
-int
-priority (char x)
+int priority(char x)
 {
   if (x == '+' || x == '-')
-    {
-      return 1;
-    }
+  {
+    return 1;
+  }
 
   if (x == '*' || x == '/' || x == '%')
-    {
-      return 2;
-    }
+  {
+    return 2;
+  }
   return 0;
 }
 
-int
-main ()
+int main()
 {
   char exp[100];
   char *e, x;
-  printf ("Enter the expression : ");
-  scanf ("%s", exp);
-  printf ("\n");
+  printf("Enter the expression : ");
+  scanf("%s", exp);
+  printf("\n");
   e = exp;
 
   while (*e != '\0')
+  {
+    if (isalnum(*e))
     {
-      if (isalnum (*e))
-	{
-	  printf ("%c ", *e);
-	}
-
-      else if (*e == '(')
-	{
-	  push (*e);
-	}
-
-      else if (*e == ')')
-	{
-	  while ((x = pop ()) != '(')
-	    {
-	      printf ("%c ", x);
-	    }
-	  // display(stack);
-	}
-      else
-	{
-	  if (priority (*e) <= priority (stack[top]))
-	    {
-	      printf ("%c ", pop ());
-	    }
-
-	  push (*e);
-	}
-      e++;
+      printf("%c ", *e);
     }
-// display(stack);
+
+    else if (*e == '(')
+    {
+      push(*e);
+    }
+
+    else if (*e == ')')
+    {
+      while ((x = pop()) != '(')
+      {
+        printf("%c ", x);
+      }
+    }
+    else
+    {
+      if (priority(*e) <= priority(stack[top]))
+      {
+        printf("%c ", pop());
+      }
+
+      push(*e);
+    }
+    e++;
+  }
   while (top != -1)
-    {
-      printf ("%c ", pop ());
-    }
-  display (stack);
-  printf ("\n");
+  {
+    printf("%c ", pop());
+  }
+
+  printf("\n");
   return 0;
 }
