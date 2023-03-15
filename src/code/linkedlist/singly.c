c29df0b0f8d61150da889abf9ed0f5cbfa7121b3
Tejas Bhovad <tejasbhovad@gmail.com>
2023-03-15 18:25:26 +0530

    Added Singly Linked List

--- src/code/linkedlist/singly.c
+++ src/code/linkedlist/singly.c
@@ -6,7 +6,7 @@ struct node
     int data;
     struct node *next;
 };
-void display(struct node *head)
+void display()
 {
     struct node *current = head;
     printf("List: ");
@@ -19,7 +19,6 @@ void display(struct node *head)
 }
 struct node *createNode()
 {
-
     newNode = (struct node *)malloc(sizeof(struct node));
     newNode->next = 0;
     if (head == 0)
@@ -95,7 +94,6 @@ struct node *addNodeBefore()
 {
     if (head == 0)
     {
-        // head = newNode
         printf("\nERROR: Linked List not Defined.");
         newNode = createNode();
         return newNode;
@@ -117,7 +115,7 @@ struct node *addNodeBefore()
     }
     return head;
 }
-void deleteNodeEnd(struct node *head)
+void deleteNodeEnd()
 {
     if (head == 0)
     {
@@ -136,7 +134,7 @@ void deleteNodeEnd(struct node *head)
         free(current);
     }
 }
-void deleteNodeStart(struct node *head)
+void deleteNodeStart()
 {
     if (head == 0)
     {
@@ -145,79 +143,122 @@ void deleteNodeStart(struct node *head)
     else
     {
         printf("\nRemoved Element: %d\n", head->data);
-        head = head->next;
-        free(head);
+        struct node *current;
+        current = head;
+        head = current->next;
+        free(current);
+    }
+}
+void deleteNodeAt()
+{
+
+    if (head == 0)
+    {
+        printf("\nERROR: No Element to Remove.");
+    }
+    else
+    {
+        int key, ctr = 0;
+        printf("\nEnter Value to be removed: ");
+        scanf("%d", &key);
+        struct node *current = head, *prev = 0;
+        while (current->data != key)
+        {
+            ctr++;
+            prev = current;
+            current = current->next;
+        }
+        
+        // when the first element is to be removed prev element is head
+        if (ctr > 1)
+        {
+            prev->next = current->next;
+        }
+        else
+        {
+            head = current->next;
+        }
+
+        free(current);
     }
 }
 int main()
 {
-    // IGNORE: TEST CASES
-    newNode = createNode();
-    head = addNodeStart();
-    display(head);
-    head = addNodeEnd();
-    display(head);
-    // head = addNodeAfter();
-    // display(head);
-    // head = addNodeBefore();
-    // display(head);
-    deleteNodeStart(head);
-    display(head);
-
-    // int choice;
-    // do
-    // {
-    //     printf("\nENTER CHOICE:");
-    //     printf("\n1. Create Node (with initial value)");
-    //     printf("\n2. Add Node at Start of List");
-    //     printf("\n3. Add Node at End of List");
-    //     printf("\n4. Add Node after key element");
-    //     printf("\n5. Add Node before key element");
-    //     printf("\n6. Exit the Program\n");
-    //     scanf("%d", &choice);
-    //     switch (choice)
-    //     {
-    //     case 1:
-    //     {
-    //         newNode = createNode();
-    //         display(head);
-    //         break;
-    //     }
-    //     case 2:
-    //     {
-    //         head = addNodeStart();
-    //         display(head);
-    //         break;
-    //     }
-    //     case 3:
-    //     {
-    //         head = addNodeEnd();
-    //         display(head);
-    //         break;
-    //     }
-    //     case 4:
-    //     {
-    //         head = addNodeAfter();
-    //         display(head);
-    //         break;
-    //     }
-    //     case 5:
-    //     {
-    //         head = addNodeBefore();
-    //         display(head);
-    //         break;
-    //     }
-    //     case 6:
-    //     {
-    //         printf("\nExited.\n");
-    //         break;
-    //     }
-    //     default:
-    //     {
-    //         printf("\nInvalid Choice");
-    //         continue;
-    //     }
-    //     }
-    // } while (choice != 6);
+    int choice;
+    do
+    {
+        printf("\nENTER CHOICE:");
+        printf("\n1. Create Node (with initial value)");
+        printf("\n2. Add Node at Start of List");
+        printf("\n3. Add Node at End of List");
+        printf("\n4. Add Node after key element");
+        printf("\n5. Add Node before key element");
+        printf("\n6. Delete Element from Start");
+        printf("\n7. Delete Element from End");
+        printf("\n8. Delete key Element");
+        printf("\n9. EXIT\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+        {
+            newNode = createNode();
+            display();
+            break;
+        }
+        case 2:
+        {
+            head = addNodeStart();
+            display();
+            break;
+        }
+        case 3:
+        {
+            head = addNodeEnd();
+            display();
+            break;
+        }
+        case 4:
+        {
+            head = addNodeAfter();
+            display();
+            break;
+        }
+        case 5:
+        {
+            head = addNodeBefore();
+            display();
+            break;
+        }
+        case 6:
+        {
+            deleteNodeStart();
+            display();
+            break;
+        }
+        case 7:
+        {
+            deleteNodeEnd();
+            display();
+            break;
+        }
+        case 8:
+        {
+            deleteNodeAt();
+            display();
+            break;
+        }
+        case 9:
+        {
+            printf("\nExited.\n");
+            break;
+        }
+        default:
+        {
+            printf("\nInvalid Choice, Try again.\n");
+            continue;
+        }
+        }
+    } while (choice != 9);
     return 0;
 }
