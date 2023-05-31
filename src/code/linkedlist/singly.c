71f6a04161e8de975f6c0931352a8ced932656f7
Tejas Bhovad <tejasbhovad@gmail.com>
2023-05-31 09:14:41 +0530

    updated singly linked list

--- src/code/linkedlist/singly.c
+++ src/code/linkedlist/singly.c
@@ -1,120 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct NODE
+struct NODE
 {
     int data;
     struct NODE *next;
-}
-typedef NODE;
+} typedef NODE;
 NODE *head = 0, *newNode;
 
 void display()
 {
-    NODE *current = head;
-    printf("List: ");
-    while (current != 0)
+    if (head != 0)
     {
-        printf("%d ", current->data);
-        current = current->next;
+        NODE *current = head;
+        printf("List: ");
+        while (current != 0)
+        {
+            printf("%d ", current->data);
+            current = current->next;
+        }
+        printf("\n");
     }
-    printf("\n");
 }
 
 void createNode()
 {
     newNode = (NODE *)malloc(sizeof(NODE));
-    if (head == 0)
-    {
-        printf("\nEmpty List created \n");
-        newNode->next = 0;
-        printf("\nEnter Value to be Added: ");
-        scanf("%d", &(newNode->data));
-        head = newNode;
-    }
-    else
-    {
-        printf("\nEnter Value to be Added: ");
-        scanf("%d", &(newNode->data));
-        newNode->next = 0;
-    }
+    newNode->next = 0;
+    printf("\nEnter Value to be Added: ");
+    scanf("%d", &(newNode->data));
 }
 
 void addNodeStart()
 {
-    if (head == 0)
-    {
-        printf("\nERROR: Linked List not Defined.\n");
-    }
-    else
-    {
-        createNode();
-        newNode->next = head;
-        head = newNode;
-    }
+    createNode();
+    newNode->next = head;
+    head = newNode;
 }
 
 void addNodeEnd()
 {
-    if (head == 0)
+    createNode();
+    if (head == NULL)
     {
-        printf("\nERROR: Linked List not Defined.\n");
+        head = newNode;
     }
-    else
+
+    NODE *current = head;
+    while (current->next != 0)
     {
-        createNode();
-        NODE *current = head;
-        while (current->next != 0)
-        {
-            current = current->next;
-        }
-        current->next = newNode;
-        newNode->next = 0;
+        current = current->next;
     }
+    current->next = newNode;
+    newNode->next = 0;
 }
 
 void addNodeAfter()
 {
-    if (head == 0)
+    int key;
+    printf("\nEnter Value to be added after: ");
+    scanf("%d", &key);
+    createNode();
+    NODE *current = head;
+    while (current->data != key)
     {
-        printf("\nERROR: Linked List not Defined.\n");
+        current = current->next;
     }
-    else
-    {
-        int key;
-        printf("\nEnter Value to be added after: ");
-        scanf("%d", &key);
-        createNode();
-        NODE *current = head;
-        while (current->data != key)
-        {
-            current = current->next;
-        }
-        newNode->next = current->next;
-        current->next = newNode;
-    }
+    newNode->next = current->next;
+    current->next = newNode;
 }
 
 void addNodeBefore()
 {
-    if (head == 0)
+    int key;
+    printf("\nEnter Value to be added before: ");
+    scanf("%d", &key);
+    createNode();
+    NODE *current = head, *prev;
+    while (current->data != key)
     {
-        printf("\nERROR: Linked List not Defined.");
+        prev = current;
+        current = current->next;
     }
-    else
-    {
-        int key;
-        printf("\nEnter Value to be added before: ");
-        scanf("%d", &key);
-        createNode();
-        NODE *current = head, *prev;
-        while (current->data != key)
-        {
-            prev = current;
-            current = current->next;
-        }
-        newNode->next = prev->next;
-        prev->next = newNode;
-    }
+    newNode->next = prev->next;
+    prev->next = newNode;
 }
 
 void deleteNodeEnd()
@@ -190,67 +158,60 @@ int main()
     do
     {
         printf("\nENTER CHOICE:");
-        printf("\n1. Create Empty List");
-        printf("\n2. Add Node at Start of List");
-        printf("\n3. Add Node at End of List");
-        printf("\n4. Add Node after key element");
-        printf("\n5. Add Node before key element");
-        printf("\n6. Delete Element from Start");
-        printf("\n7. Delete Element from End");
-        printf("\n8. Delete key Element");
-        printf("\n9. EXIT\n");
+        printf("\n1. Add Node at Start of List");
+        printf("\n2. Add Node at End of List");
+        printf("\n3. Add Node after key element");
+        printf("\n4. Add Node before key element");
+        printf("\n5. Delete Element from Start");
+        printf("\n6. Delete Element from End");
+        printf("\n7. Delete key Element");
+        printf("\n8. EXIT\n");
         scanf("%d", &choice);
         switch (choice)
         {
         case 1:
         {
-            createNode();
+            addNodeStart();
             display();
             break;
         }
         case 2:
         {
-            addNodeStart();
+            addNodeEnd();
             display();
             break;
         }
         case 3:
         {
-            addNodeEnd();
+            addNodeAfter();
             display();
             break;
         }
         case 4:
         {
-            addNodeAfter();
+            addNodeBefore();
             display();
             break;
         }
         case 5:
         {
-            addNodeBefore();
+            deleteNodeStart();
             display();
             break;
         }
         case 6:
         {
-            deleteNodeStart();
+            deleteNodeEnd();
             display();
             break;
         }
         case 7:
         {
-            deleteNodeEnd();
-            display();
-            break;
-        }
-        case 8:
-        {
             deleteNodeAt();
             display();
             break;
         }
-        case 9:
+        case 8:
         {
             printf("\nExited.\n");
             break;
@@ -261,6 +222,6 @@ int main()
             continue;
         }
         }
-    } while (choice != 9);
+    } while (choice != 8);
     return 0;
 }
