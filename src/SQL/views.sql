3c9860f8b33eb51f6834d4c51f8ae257b259c1fb
Tejas Bhovad <tejasbhovad@gmail.com>
2023-10-29 19:07:58 +0530

    small changes

--- src/SQL/views.sql
+++ src/SQL/views.sql
@@ -8,9 +8,17 @@ CREATE TABLE Employees(
 INSERT INTO Employees VALUES(1,'Rohit',300);
 INSERT INTO Employees VALUES(2,'Munawar',1200);
 
--- CREATING VIEW
+-- > CREATING VIEW
 CREATE VIEW Client AS
 SELECT name,salary
 FROM Employees;
 -- SELECTING VIEW
-SELECT* FROM Client;
+SELECT * FROM Client;
+-- > DELETING A VIEW 
+-- DELETE FROM view_name WHERE condition;
+
+-- > DROP A VIEW
+-- DROP VIEW Client;
+
+-- > UPDATING A VIEW
+-- UPDATE view_name SET column1 = value1 WHERE condition;
